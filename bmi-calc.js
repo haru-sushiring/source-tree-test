d3b8aadb158c4a0329e0b5b35754fcda281cabfc
ITS-58\大塚暖生 <haruki.otsuka@g.dreamcareer.co.jp>
2023-05-15 16:52:23 +0900

    my-branch

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,4 +1,4 @@
-function calcBmi() {
+function calcBmi1() {
     var height = document.getElementById("height").value
     var weight = document.getElementById("weight").value
     var bmi = weight / Math.pow(height / 100, 2)
